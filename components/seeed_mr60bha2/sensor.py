2b71fdc046a36930abc8e54bfc2be8b4cd3f82d6
limengdu <37475446+limengdu@users.noreply.github.com>
2024-10-10 11:23:41 +0800

    Fix

--- components/seeed_mr60bha2/sensor.py
+++ components/seeed_mr60bha2/sensor.py
@@ -4,14 +4,14 @@ import esphome.config_validation as cv
 from esphome.const import (
     DEVICE_CLASS_DISTANCE,
     UNIT_CENTIMETER,
+    CONF_DISTANCE,
 )
 from . import CONF_MR60BHA2_ID, MR60BHA2Component
 
 AUTO_LOAD = ["seeed_mr60bha2"]
 
 CONF_BREATH_RATE = "breath_rate"
-CONF_HEART_RATE= "heart_rate"
-CONF_DISTANCE = "distance"
+CONF_HEART_RATE = "heart_rate"
 
 CONFIG_SCHEMA = cv.Schema(
     {
